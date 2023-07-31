e2dd64fe2d1f2e43e07f7f403edeb84491e27406
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2023-07-31 13:32:07 +0100

    Add: add css element to design

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import  './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const fname = "Oluwadamilare";
+const lname = "Ezekiel";
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <h1 className = "name" contentEditable = "false">
+      Hello World: I am {` ${lname} ${fname}`}
+    </h1>
+    <ul>
+      <li>
+        Puffer
+      </li>
+      <li>
+        Juice
+      </li>
+      <li>
+        Dough
+      </li>
+    </ul>
+
+  
+    <div>
+
+      <img 
+      className='images' 
+      src="my-react-app\src\DADDY.jpg"
+      alt= "daddy"
+      />
+
+      <img 
+      className='images' 
+      src="my-react-app\src\Dami E Headshot.jpeg"
+      alt= "damilare"
+      />
+
+    </div>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
