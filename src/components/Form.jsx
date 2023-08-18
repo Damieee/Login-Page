a1865db0fc8bd467f54e6587404ffe35bc51412f
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2023-08-18 14:54:13 +0100

    Fix: remove redundant code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
-import MyButton from './Button'; // Update the import path
+import MyButton from './Button'; // Update the import path for MyButton component
 
 function MyForm() {
+  // State to hold form data
   const [formData, setFormData] = useState({
     username: '',
     password: ''
   });
 
+  // Constant to update form data when inputs change
   const handleInputChange = (event) => {
     const { id, value } = event.target;
+    // Update the state with the new value while keeping the rest unchanged
     setFormData((prevData) => ({
       ...prevData,
       [id]: value
     }));
   };
 
+  // Function to handle form submission
   const handleSubmit = () => {
     console.log('Form data:', formData);
   };
@@ -23,6 +27,7 @@ function MyForm() {
   return (
     <div>
       <Form>
+        {/* Username input */}
         <Form.Group controlId="formUsername">
           <Form.Label>Username</Form.Label>
           <Form.Control
@@ -32,6 +37,7 @@ function MyForm() {
             onChange={handleInputChange}
           />
         </Form.Group>
+        {/* Password input */}
         <Form.Group controlId="formPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control
@@ -41,6 +47,7 @@ function MyForm() {
             onChange={handleInputChange}
           />
         </Form.Group>
+        {/* Button component for form submission */}
         <MyButton onSubmit={handleSubmit} />
       </Form>
     </div>
