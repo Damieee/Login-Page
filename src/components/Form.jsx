0c8022f70a405e27ac51e2389c5998d33483954a
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2023-08-30 11:41:41 +0100

    Fix:Fix errors

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { Form } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-
-
-
-function onClickEvent(event, onSubmit) {
-
-  alert('User Logged in');
-=======
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 
 function MyForm() {
   // State to hold form data
-
   const [email, setEmail] = useState("");
-
   const [password, setPassword] = useState("");
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log("email", email, "password", password);
+    alert(`email: ${email}, password: ${password}`);
   };
 
   return (
@@ -53,89 +40,6 @@ function MyForm() {
       </Button>
     </Form>
   );
->>>>>>> 92ef3f43fd6d7111801dca2822ef6c2449990c56
 }
-
-function MyForm() {
-  // State to hold form data
-
-  const [userEmail, setuserEmail] = useState("");
-  
-  const [userPassword, setuserPassword] = useState("");
-
-  const onInputChange = (setState) => (event) => {
-    setState(event.target.value);
-  };
-
-  // Function to handle form submission
-  const handleSubmit = 
-  () => async (event, userEmail, userPassword) => {
-    event.preventDefault();
-
-    console.log(`userEmail: ${userEmail}, Password: ${userPassword}`);
-
-
-
-
-    // Clear previous error messages
-    setuserEmail("");
-    setuserPassword("");
-
-      if (!userEmail || userEmail.trim().length === 0 || !userEmail.includes("@")) {
-        setuserEmail("Invalid email");
-      }
-
-      if (!userPassword || userPassword.trim().length < 6) {
-        setuserPassword("Invalid password");
-      }
-
-      if (userEmail === "Invalid email" || userPassword === "Invalid password") {
-    return;
-      }
-
-    
-  };
-
-
-
-  return (
-    <div>
-      <Form>
-        {/* Username input */}
-        <Form.Group controlId="formUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter your username"
-            value={userEmail}
-            onChange={onInputChange(setuserEmail)}
-          
-          />
-        </Form.Group>
-        {/* Password input */}
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter your password"
-            value={userPassword}
-            onChange= {onInputChange(setuserPassword)}
-            onSubmit={handleSubmit}
-          />
-        </Form.Group>
-        {/* Button component for form submission */}
-        <Button 
-            variant="primary" 
-            type="submit" 
-            className="w-100" 
-            onClick={(event, onSubmit) => onClickEvent(event, onSubmit)}
-            >
-            Login
-          </Button>
-      </Form>
-    </div>
-  );
-};
-
 
 export default MyForm;
