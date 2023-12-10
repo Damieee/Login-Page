05cadb5094a8ae9faa7a5f1134afa54fa1abfbd8
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2023-12-10 18:40:08 +0100

    Refactored Code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { Container, Row, Col, Form} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import MyForm from './Form';
-import Header from './Header';
 
 
 function App() {
-  return (
-    <Container className="d-flex justify-content-center align-items-center vh-100">
-      <Row className="w-100">
-        <Col md={4} className="mx-auto">
-          <Form>
-            <Header/>
-            <MyForm/>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+  return(
+      <MyForm/>
   );
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Form } from "react-bootstrap";
-import { Button } from "react-bootstrap";
+import { React, useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyForm() {
   // State to hold form data
@@ -13,33 +13,42 @@ function MyForm() {
   };
 
   return (
-    <Form onSubmit={submitHandler}>
-      {/* Username input */}
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter your username"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </Form.Group>
-      {/* Password input */}
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Enter your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Form.Group>
-      {/* Button component for form submission */}
-      <Button variant="primary" type="submit" className="w-100">
-        Login
-      </Button>
-    </Form>
-  );
+  
+        <Container className="d-flex justify-content-center align-items-center vh-100">
+          <Row className="w-100">
+            <Col md={4} className="mx-auto">
+              <h2 className="mb-4 text-center">Login</h2> 
+              <Form onSubmit={submitHandler}>
+                {/* Username input */}
+                <Form.Group controlId="formUsername">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter your username"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Form.Group>
+                {/* Password input */}
+                <Form.Group controlId="formPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Form.Group>
+                {/* Button component for form submission */}
+                <Button variant="primary" type="submit" className="w-100 mt-4">
+                  Login
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+  
+      );
 }
 
 export default MyForm;
